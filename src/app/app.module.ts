ff863cea350d51b776b6d7644483ac361403e37d
Nomakhosi <nomakhosi.ndebele@dariel.co.za>
2019-04-03 08:42:07 +0200

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ListMoviesComponent } from './components/list-movies/list-movies.component';
+import { EditMovieComponent } from './components/edit-movie/edit-movie.component';
+import { AddMovieComponent } from './components/add-movie/add-movie.component';
+import { DeleteMovieComponent } from './components/delete-movie/delete-movie.component';
+import {HttpClientModule} from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListMoviesComponent,
+    EditMovieComponent,
+    AddMovieComponent,
+    DeleteMovieComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
